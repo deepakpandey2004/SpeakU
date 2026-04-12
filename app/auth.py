c6d6fcf7ebbb8f3c170478482f5d3a8d9904011a
Deepak Pandey <whynotdeepaks@gmail.com>
2026-04-12 16:24:57 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
-# Password hashing engine
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__rounds=12)
 
 def hash_password(password: str) -> str:
